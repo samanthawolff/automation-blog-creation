8512b8e0fb3cda2b99bebc3ed52297769297e2b7
Samantha Wolff <samanthawolff07@gmail.com>
2022-05-11 18:38:28 -0500

    set up Post model

--- models/Index.js
+++ models/Index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
 
 User.hasMany(Post, {
@@ -8,6 +9,22 @@ User.hasMany(Post, {
 
 Post.belongsTo(User, {
     foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
 });
 
 
--- models/Post.js
+++ models/Post.js
@@ -19,7 +19,10 @@ Post.init(
         },
         post_text: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
         },
         user_id: {
             type: DataTypes.INTEGER,
