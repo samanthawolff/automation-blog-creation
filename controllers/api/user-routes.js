7a2a2ddb0665b03329c17476c1ceaf8e0b7449eb
Samantha Wolff <samanthawolff07@gmail.com>
2022-05-11 13:55:35 -0500

    working update and delete user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -49,4 +49,46 @@ router.post('/', (req, res) => {
 });
 
 
+// Update a user by ID
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData[0]) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+// Delete a user by ID
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
 module.exports = router;
