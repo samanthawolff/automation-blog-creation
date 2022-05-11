098be34570bf5b1b140596c6cc65f38d807399c3
Samantha Wolff <samanthawolff07@gmail.com>
2022-05-11 14:21:51 -0500

    Post model and user login validation

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -52,6 +52,28 @@ router.post('/', (req, res) => {
 });
 
 
+// Login route
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with this email address' });
+            return;
+        }
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password' });
+            return;
+        }
+        res.json({ user: dbUserData, message: 'Logged In!'});
+    });
+});
+
+
 // Update a user by ID
 router.put('/:id', (req, res) => {
     User.update(req.body, {
