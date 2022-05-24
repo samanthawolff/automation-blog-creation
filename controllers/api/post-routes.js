1586a02c2d5de1c8cefe1d5d236907c974f0ac18
Samantha Wolff <samanthawolff07@gmail.com>
2022-05-24 17:59:19 -0500

    set up handlebars

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
         attributes: ['id', 'post_text', 'title', 'created_at'],
         attributes: [
             'id', 
-            'post_url', 
+            'post_text', 
             'title', 
             'created_at',
             [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
@@ -46,7 +46,7 @@ router.get('/:id', (req, res) => {
         attributes: ['id', 'post_text', 'title', 'created_at'],
         attributes: [
             'id', 
-            'post_url', 
+            'post_text', 
             'title', 
             'created_at',
             [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
--- models/Post.js
+++ models/Post.js
@@ -15,7 +15,7 @@ class Post extends Model {
                 },
                 attributes: [
                     'id',
-                    'post_url',
+                    'post_text',
                     'title',
                     'created_at',
                     [
